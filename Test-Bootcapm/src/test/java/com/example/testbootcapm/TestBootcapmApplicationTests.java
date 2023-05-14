55d881b32fa7f3e0ae90b9c73c8a3d31b6141ff0
m.a السلمي <doodg16@hotmail.com>
2023-05-14 14:18:07 +0300

    Frist Project

---
+++ Test-Bootcapm/src/test/java/com/example/testbootcapm/TestBootcapmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testbootcapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBootcapmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
